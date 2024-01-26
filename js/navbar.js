64ed27c278af43b9a79bb9076e75ff67a6172a66
Azim4ik_06 <saidmurodovazim808@gmail.com>
2024-01-26 21:10:58 +0500

    добавление адаптации

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,11 @@
 const navbarToggle = document.querySelector('.navbar-toggler')
 const collapse = document.querySelector('.collapse')
+const navTitle = document.querySelector('.nav-title')
 
 navbarToggle.addEventListener('click', navbarShow)
 function navbarShow() {
     collapse.classList.toggle('show')
+    navTitle.classList.toggle('d-none')
 }
 
 const dropDown = document.querySelector('.dropdown-toggle')
@@ -13,3 +15,4 @@ dropDown.addEventListener('click', dropShow)
 function dropShow() {
     dropMenu.classList.toggle('show')
 }
+
