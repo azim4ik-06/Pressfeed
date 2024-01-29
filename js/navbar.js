a4dbbfd106143cdbf4ca88d31a6ddf4ae9138199
Azim4ik_06 <saidmurodovazim808@gmail.com>
2024-01-29 15:50:32 +0500

    убран иконка подарка во время нажатии на Navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,14 @@
 const navbarToggle = document.querySelector('.navbar-toggler')
 const collapse = document.querySelector('.collapse')
 const navTitle = document.querySelector('.nav-title')
+const surpriseBtn = document.querySelector('.btn-surprise')
 const navbarBrand = document.querySelector('.navbar-brand') 
 
 navbarToggle.addEventListener('click', navbarShow)
 function navbarShow() {
     collapse.classList.toggle('show')
     navTitle.classList.toggle('d-none')
+    surpriseBtn.classList.toggle('d-none')
     navbarBrand.classList.toggle('d-none')
 }
 
